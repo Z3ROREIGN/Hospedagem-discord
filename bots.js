6b668aeacb3b65f89af690cd54995c6de476a54e
Z3ROREIGN <149897659+Z3ROREIGN@users.noreply.github.com>
2025-10-17 08:43:01 -0300

    Add configuration for multiple bots

---
+++ bots.js
@@ -0,0 +1,12 @@
+{
+    "bot_token_1": {
+        "owner_id": 123456789012345678,
+        "permissions": [],
+        "bot_limit": 0
+    },
+    "bot_token_2": {
+        "owner_id": 987654321098765432,
+        "permissions": [],
+        "bot_limit": 0
+    }
+}
